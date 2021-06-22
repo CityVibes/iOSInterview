b9088fc0b45d76c33ed87aad31beecf7d94693bb
Julius Skripkauskas <ext.jskripkauskas@riotgames.com>
2021-06-22 07:35:18 -0700

    Initial Commit

---
+++ Jams/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jams
+//
+//  Created by Julius Skripkauskas on 6/22/21.
+//  Copyright © 2021 Julius Skripkauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
